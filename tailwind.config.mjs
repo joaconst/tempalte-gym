8e6a068f94159b11ed8627fe45e5d2e3ec43fe5f
joacnt <constabeljoaquinn@gmail.com>
2025-02-28 15:54:18 -0300

    Initial commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#FF4C29',
+        secondary: '#082032',
+        accent: '#2C394B',
+        light: '#F0F0F0',
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        heading: ['Montserrat', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
